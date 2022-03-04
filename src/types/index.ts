07661c882b583447697dca4bb1332ae9e0ace969
akhil-chandran <akhil.chandran@neoito.com>
2022-03-04 11:05:05 +0530

    feat: add type UpperSnakecase

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,12 +3,15 @@ export type Nullable<T> = T | null;
 
 /**
  * Used to override default function signatures
- *
- * @example:
+ * 
+ * @param BaseFunction - The function to override
+ * @param T - Custom arguments
+ * 
+ * @example
  * const calculateAreaOfSquare: (a: number) => number = (a: number) => a * a;
  * const calculateAreaofRectangle: DerivedFunction<typeof calculateAreaOfSquare, [number]> = (a: number, b: number) => a * b;
  * typeof calculateAreaofRectangle // (a: number, b: number) => number
- *
+ * 
  * */
 export type DerivedFunction<
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -16,6 +19,27 @@ export type DerivedFunction<
   T extends unknown[] = unknown[]
 > = BaseFunction extends (...a: infer U) => infer R
   ? (...a: [...U, ...T]) => R
+  : never;
+
+/**
+ * Converts string or template literals to UPPER_SNAKE_CASE
+ * 
+ * @param S - String or template literal
+ * @param D - Delimiter
+ * 
+ * @example
+ * type upperSnakeCase = toUpperSnakeCase('hello world'); // HELLO_WORLD
+ * type upperSnakeCase = toUpperSnakeCase('hello:world | test:arg', ':'); // HELLO_WORLD | TEST_ARG
+ * 
+ */
+
+export type UpperSnakecase<
+  S extends string,
+  D extends string = ''
+> = S extends `${infer FirstWord}${D}${infer Rest}`
+  ? D extends ''
+    ? Uppercase<S>
+    : `${Uppercase<FirstWord>}_${Uppercase<Rest>}`
   : never;
 
 // Generic type for generator function that returns void
