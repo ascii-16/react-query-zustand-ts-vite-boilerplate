f62faa9ea5c7384ded328a203730811ddc7d815c
akhil-chandran <akhil.chandran@neoito.com>
2022-05-31 11:28:02 +0530

    refactor: button default classes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,8 +8,23 @@ export interface ButtonProps
   children?: React.ReactNode;
 }
 
-const defaultClasses =
-  'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 min-w-[100px]';
+const defaultClasses = `
+    text-white
+    bg-blue-700
+    hover:bg-blue-800 
+    focus:ring-4 
+    focus:ring-blue-300 
+    font-medium 
+    rounded-lg 
+    text-sm 
+    px-5 py-2.5 
+    text-center 
+    mr-2 mb-2
+    dark:bg-blue-600
+    dark:hover:bg-blue-700
+    dark:focus:ring-blue-800 
+    min-w-[100px]
+  `;
 
 const Button: React.FC<ButtonProps> = ({
   text,
