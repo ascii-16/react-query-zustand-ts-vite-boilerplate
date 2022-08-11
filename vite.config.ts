017fa089e9a3280d60bb0ca10d1b7bb48ee07e67
akhil-chandran <akhil.chandran@neoito.com>
2022-08-11 18:38:43 +0530

    feat: integrate pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
+import { PWAConfig } from './src/lib/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), VitePWA(PWAConfig)],
 });
