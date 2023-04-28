c885da4864648647aad4af3e9da246525e0cd3ff
akhil-neoito <akhil.chandran@neoito.com>
2023-04-28 16:15:04 +0530

    chore: update to vite swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { VitePWA } from 'vite-plugin-pwa';
 import { PWAConfig } from './src/lib/config';
