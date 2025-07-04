37cde8dd05ca32efed927bf5342626c484971962
ascii-16 <mail.akhil99@gmail.com>
2025-07-04 18:55:20 +0530

    feat: upgrade tailwind to v4

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './styles.css';
+import './index.css';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,14 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { PWAConfig } from './src/lib/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), VitePWA(PWAConfig)],
+  css: {
+    devSourcemap: true,
+  },
+  plugins: [react(), tsconfigPaths(), tailwindcss(), VitePWA(PWAConfig)],
 });
