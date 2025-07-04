7e1a38d8601c19ded7167514b7f7145a78fdacac
ascii-16 <mail.akhil99@gmail.com>
2025-07-04 20:51:51 +0530

    fix: build error

--- src/store/logger.ts
+++ src/store/logger.ts
@@ -3,24 +3,25 @@ import { type StateCreator, type StoreMutatorIdentifier } from 'zustand';
 type Logger = <
   T,
   Mps extends Array<[StoreMutatorIdentifier, unknown]> = [],
-  Mcs extends Array<[StoreMutatorIdentifier, unknown]> = []
+  Mcs extends Array<[StoreMutatorIdentifier, unknown]> = [],
 >(
   f: StateCreator<T, Mps, Mcs>,
-  name?: string
+  name?: string,
 ) => StateCreator<T, Mps, Mcs>;
 
 type LoggerImpl = <T>(
   f: StateCreator<T, [], []>,
-  name?: string
+  name?: string,
 ) => StateCreator<T, [], []>;
 
 const loggerImpl: LoggerImpl = (f, name) => (set, get, store) => {
-  const loggedSet: typeof set = (...a) => {
-    set(...a);
+  const loggedSet: typeof set = (partial, replace) => {
+    set(partial, replace as false | undefined);
     if (import.meta.env.DEV) {
       console.log(...(name ? [`${name}:`] : []), get());
     }
   };
+
   store.setState = loggedSet;
 
   return f(loggedSet, get, store);
